e37858254f98af7b6d68dfa1df8107f2e6369293
rin.watanabe <RIN.WATANABE@grp.tmnf.jp>
2026-06-02 15:45:03 +0900

    categoryのエンティティ完了

--- src/main/java/com/example/entity/Category.java
+++ src/main/java/com/example/entity/Category.java
@@ -1,5 +1,40 @@
 package com.example.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length =  255, nullable = false)
+    @NotBlank(message = "カテゴリ名は必須です")
+    @Size(max = 255)
+    private String categoryName;
+    
+    @Column(length = 255, nullable = false)
+    @NotBlank(message = "単位は必須です")
+    @Size(max = 255)
+    private String unit;
+    
+    @Column(nullable = false)
+    private List<Food> foods;
 
 }
