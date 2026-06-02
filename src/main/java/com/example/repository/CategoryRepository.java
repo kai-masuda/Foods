a70aedc6a42341ee2d39ffa3092d41b6cec1dbfd
kai.masuda <user075.dounyu2026@tmns-3ssscom.net>
2026-06-02 14:48:26 +0900

    repository追加

--- src/main/java/com/example/repository/CategoryRepository.java
+++ src/main/java/com/example/repository/CategoryRepository.java
@@ -1,5 +1,13 @@
 package com.example.repository;
 
-public interface CategoryRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+    Optional<Category> findByCategoryName(String CategoryName);
+    boolean existsByCategoryName(String CategoryName);
 
 }
--- src/main/java/com/example/repository/FoodRepository.java
+++ src/main/java/com/example/repository/FoodRepository.java
@@ -1,5 +1,13 @@
 package com.example.repository;
 
-public interface FoodRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entity.Food;
+
+public interface FoodRepository extends JpaRepository<Food, Long>{
+    Optional<Food> findByFoodName(String foodName);
+    boolean existsByFoodName(String foodName);
 
 }
