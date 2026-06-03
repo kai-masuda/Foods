68b9e909b719068d426658a28c46c879bbddc6fe
rin.watanabe <RIN.WATANABE@grp.tmnf.jp>
2026-06-03 13:20:27 +0900

    修正完了

--- src/main/java/com/example/entity/Food.java
+++ src/main/java/com/example/entity/Food.java
@@ -10,13 +10,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-<<<<<<< HEAD
-=======
 import jakarta.persistence.PrePersist;  // 【追加】
 import jakarta.persistence.PreUpdate;  // 【追加】
->>>>>>> branch 'master' of https://github.com/kai-masuda/Foods.git
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import org.springframework.data.annotation.CreatedDate;
@@ -42,25 +38,19 @@ public class Food {
     private String foodName;
     
     @Column(nullable = false)
-<<<<<<< HEAD
-    @NotBlank(message = "数量は必須です")
+
+    @NotBlank(message = "量は必須です")
     @Size(max = 255)
     private String amount; // Integerだと1/4などが扱えなくなるためString型
-=======
-    @NotNull(message = "量は必須です")
-    private Integer amount;
->>>>>>> branch 'master' of https://github.com/kai-masuda/Foods.git
+
     
     @Column(nullable = false)
     private LocalDate taste_limit;
     
-<<<<<<< HEAD
+
     // @ManyToOneで複数の食材が1つのカテゴリに属する(多対1)
     @ManyToOne
     // @JoinColumnで外部キーであるcategory_idをカラム名として指定
-=======
-    @ManyToOne
->>>>>>> branch 'master' of https://github.com/kai-masuda/Foods.git
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
     
