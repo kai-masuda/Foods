dbca559d64a49d69b92587f8252a1b8f4e6a6631
yosuke.tanabe <user054.dounyu2026@tmns-3ssscom.net>
2026-06-11 09:49:05 +0900

    Revert "なに変更したか覚えてないから一旦コミット"
    This reverts commit dfe85badf3cf1febd798095d608a584509db8ed6.

--- src/main/java/com/example/controller/FoodController.java
+++ src/main/java/com/example/controller/FoodController.java
@@ -299,10 +299,6 @@ public class FoodController {
             String username = principal.getName();
             User currentUser = userService.findByUsername(username);
             if (currentUser != null) {
-                
-                //修正部分
-                Sort defaultSort = Sort.by(Sort.Direction.ASC, "id");
-                
                 // ユーザーIDに紐づく食材リストをすべて取得
                 List<Food> allFoods = foodRepository.findByUserId(currentUser.getId(), org.springframework.data.domain.Sort.unsorted());
 
